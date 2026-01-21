89559538c456102c1ef74b15c12aa30bec6fb5da
seonwoong <dltjsdnd1024@gmail.com>
2026-01-21 20:13:09 +0900

    chore: remove previous implementation

--- src/apis/client.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://127.0.0.1:8000", // 개발용
-  // 나중에: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000"
-});
-
-export default api;
